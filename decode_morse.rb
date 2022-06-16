5d612d5268702ad1e93b863d0453233b345bb4f8
sainingo <nickmentor421@gmail.com>
2022-06-16 15:33:16 +0300

    Fix linter error

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,61 +1,55 @@
 require 'set'
 
-dictionary = Hash.new
+dictionary = {}
 
-dictionary[" "] = " " 
-dictionary["A"] = ".-" 
-dictionary["B"] = "-..." 
-dictionary["C"] = "-.-." 
-dictionary["D"] = "-.." 
-dictionary["E"] = "." 
-dictionary["F"] = "..-." 
-dictionary["G"] = "--." 
-dictionary["H"] = "...." 
-dictionary["I"] = ".." 
-dictionary["J"] = ".---" 
-dictionary["K"] = "-.-" 
-dictionary["L"] = ".-.." 
-dictionary["M"] = "--" 
-dictionary["N"] = "-." 
-dictionary["O"] = "---" 
-dictionary["P"] = ".--." 
-dictionary["Q"] = "--.-" 
-dictionary["R"] = ".-." 
-dictionary["S"] = "..." 
-dictionary["T"] = "-" 
-dictionary["U"] = "..-" 
-dictionary["V"] = "...-" 
-dictionary["W"] = ".--" 
-dictionary["X"] = "-..-" 
-dictionary["Y"] = "-.--" 
-dictionary["Z"] = "--.." 
-
-dictionary["1"] = ".----" 
-dictionary["2"] = "..---" 
-dictionary["3"] = "...--" 
-dictionary["4"] = "....-" 
-dictionary["5"] = "....." 
-dictionary["6"] = "-...." 
-dictionary["7"] = "--..." 
-dictionary["8"] = "---.." 
-dictionary["9"] = "----." 
-dictionary["0"] = "-----"
-
+dictionary[' '] = ' '
+dictionary['A'] = '.-'
+dictionary['B'] = '-...'
+dictionary['C'] = '-.-.'
+dictionary['D'] = '-..'
+dictionary['E'] = '.'
+dictionary['F'] = '..-.'
+dictionary['G'] = '--.'
+dictionary['H'] = '....'
+dictionary['I'] = '..'
+dictionary['J'] = '.---'
+dictionary['K'] = '-.-'
+dictionary['L'] = '.-..'
+dictionary['M'] = '--'
+dictionary['N'] = '-.'
+dictionary['O'] = '---'
+dictionary['P'] = '.--.'
+dictionary['Q'] = '--.-'
+dictionary['R'] = '.-.'
+dictionary['S'] = '...'
+dictionary['T'] = '-'
+dictionary['U'] = '..-'
+dictionary['V'] = '...-'
+dictionary['W'] = '.--'
+dictionary['X'] = '-..-'
+dictionary['Y'] = '-.--'
+dictionary['Z'] = '--..'
+dictionary['1'] = '.----'
+dictionary['2'] = '..---'
+dictionary['3'] = '...--'
+dictionary['4'] = '....-'
+dictionary['5'] = '.....'
+dictionary['6'] = '-....'
+dictionary['7'] = '--...'
+dictionary['8'] = '---..'
+dictionary['9'] = '----.'
+dictionary['0'] = '-----'
 
 # text = "this is morse code"
-print "Enter Morse code :"
-text = gets.chomp();
+print 'Enter Morse code :'
+text = gets.chomp
 
 puts "Message: #{text}"
+puts
+morse = ''
 
-puts 
-
-morse = ""
-
-
-for c in text.split("") 
-
-    morse += dictionary[ c.upcase ].to_s + " "
+text.chars.each do |c|
+  morse += dictionary[c.upcase].to_s # {' '}
 end
 
 puts "Decoded Message: #{morse}"
